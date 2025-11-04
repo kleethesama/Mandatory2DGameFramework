53d46d0042844351b1ee665ea99ff4419e5d8ba2
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-11-04 05:26:23 +0100

    Add project files.

---
+++ Mandatory2DGameFramework/worlds/World.cs
@@ -0,0 +1,34 @@
+﻿using Mandatory2DGameFramework.model.Cretures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.worlds
+{
+    public class World
+    {
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+
+
+        // world objects
+        private List<WorldObject> _worldObjects;
+        // world creatures
+        private List<Creature> _creatures;
+
+        public World(int maxX, int maxY)
+        {
+            MaxX = maxX;
+            MaxY = maxY;
+            _worldObjects = new List<WorldObject>();
+            _creatures = new List<Creature>();
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(MaxX)}={MaxX.ToString()}, {nameof(MaxY)}={MaxY.ToString()}}}";
+        }
+    }
+}
