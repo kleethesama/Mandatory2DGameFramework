9d4ad24e3ef085b21e0ada9cc27fcfba7a768ec9
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-11-08 14:09:53 +0100

    Redoing AI behavior states.

---
+++ Mandatory2DGameFramework/Model/CreatureBehavior/AiBehaviorState.cs
@@ -0,0 +1,17 @@
+﻿using Mandatory2DGameFramework.Model.Creatures;
+
+namespace Mandatory2DGameFramework.Model.CreatureBehavior;
+
+public abstract class AiBehaviorState(AiCreature aiCreature)
+{
+    protected AiCreature _aiCreature = aiCreature;
+
+    public abstract string Description { get; }
+
+    public abstract void ExecuteBehavior();
+
+    public override string ToString()
+    {
+        return Description;
+    }
+}
--- Mandatory2DGameFramework/Model/CreatureBehavior/IdleBirdBehavior.cs
+++ Mandatory2DGameFramework/Model/CreatureBehavior/IdleBirdBehavior.cs
@@ -2,8 +2,15 @@
 
 namespace Mandatory2DGameFramework.Model.CreatureBehavior;
 
-public class BirdBehavior : IBehavior
+public class IdleBirdBehavior(AiCreature aiCreature) : AiBehaviorState(aiCreature)
 {
+    public override string Description => "";
+
+    public override void ExecuteBehavior()
+    {
+        throw new NotImplementedException();
+    }
+
     public void NpcReaction(Creature creature)
     {
         throw new NotImplementedException();
