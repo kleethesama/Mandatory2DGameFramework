59db6b9786a21024288f9903c09e213218218c25
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-11-08 15:23:06 +0100

    WIP basic AI bird behavior.

--- Mandatory2DGameFramework/Model/CreatureBehavior/IdleBirdBehavior.cs
+++ Mandatory2DGameFramework/Model/CreatureBehavior/IdleBirdBehavior.cs
@@ -1,23 +1,35 @@
 ﻿using Mandatory2DGameFramework.Model.Creatures;
+using Mandatory2DGameFramework.Worlds;
 
 namespace Mandatory2DGameFramework.Model.CreatureBehavior;
 
 public class IdleBirdBehavior(AiCreature aiCreature) : AiBehaviorState(aiCreature)
 {
-    public override string Description => "";
+    public override string Description => "Behavior for when a bird is idle.";
 
     public override void ExecuteBehavior()
     {
         throw new NotImplementedException();
     }
 
-    public void NpcReaction(Creature creature)
+    public void Idle(Creature creature)
     {
         throw new NotImplementedException();
     }
 
-    public void PlayerReaction(Creature creature)
+    public bool TryScanDetectRange(out Creature? detectedCreature)
     {
-        throw new NotImplementedException();
+        for (int i = 1; i < _aiCreature.DetectRange; i++)
+        {
+            for (int j = 1; j < _aiCreature.DetectRange; j++)
+            {
+                WorldPosition detectPosition1 = _aiCreature.Position + new WorldPosition(i, j);
+                WorldPosition detectPosition2 = _aiCreature.Position + new WorldPosition(j, i);
+                // Check if a creature is in tile.
+                throw new NotImplementedException();
+            }
+        }
+        detectedCreature = null;
+        return false;
     }
 }
