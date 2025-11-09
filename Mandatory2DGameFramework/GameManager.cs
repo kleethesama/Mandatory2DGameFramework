a605a765d163becd6c998567c7e00a8b8571edf0
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-11-09 16:16:53 +0100

    Making GameManager static instead of singleton.

---
+++ Mandatory2DGameFramework/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using Mandatory2DGameFramework.Config;
+using Mandatory2DGameFramework.Worlds;
+
+namespace Mandatory2DGameFramework;
+
+public static class GameManager
+{
+    //private static GameManager Instance;
+    private static ConfigManager ConfigManager;
+    private static WorldManager WorldManager;
+
+    //private GameManager() { }
+
+    //public static GameManager GetInstance()
+    //{
+    //    Instance ??= new GameManager();
+    //    ConfigManager ??= ConfigManager.GetInstance();
+    //    WorldManager ??= WorldManager.GetInstance();
+    //    return Instance;
+    //}
+
+    public static void Start()
+    {
+        ConfigManager ??= ConfigManager.GetInstance();
+        WorldManager ??= WorldManager.GetInstance();
+        Main();
+    }
+
+    private static void Main()
+    {
+
+    }
+}
