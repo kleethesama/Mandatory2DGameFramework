734ebbb1124eca9217e66c94bbe3dbed20c35156
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-11-09 17:11:34 +0100

    These base setter classes are probably a bit overkill.

--- Mandatory2DGameFramework/Config/ConfigSetterWorker.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mandatory2DGameFramework.Config;
-
-public abstract class ConfigSetterWorker<T>(ConfigReaderWorker<T> reader)
-{
-    public ConfigReaderWorker<T> Reader { get; protected set; } = reader;
-
-    public abstract void Set(ref T reference, T value);
-}
--- Mandatory2DGameFramework/Config/WorldSizeConfigSetter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mandatory2DGameFramework.Config;
-
-public class WorldSizeConfigSetter(ConfigReaderWorker<int[]> reader) : ConfigSetterWorker<int[]>(reader)
-{
-    public override void Set(ref int[] reference, int[] value)
-    {
-        reference = value;
-    }
-}
