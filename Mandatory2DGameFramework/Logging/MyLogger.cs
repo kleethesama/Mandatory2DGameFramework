c7351faf0c2380cf15e78227dab08f25015ec2a7
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-11-14 21:32:34 +0100

    Setup MyLogger to work on further.

---
+++ Mandatory2DGameFramework/Logging/MyLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace Mandatory2DGameFramework.Logging;
+
+public sealed class MyLogger
+{
+    private static readonly Lazy<MyLogger> _instance = new(() => new MyLogger());
+
+    public static MyLogger Instance { get => _instance.Value; }
+
+    private MyLogger() { }
+}
