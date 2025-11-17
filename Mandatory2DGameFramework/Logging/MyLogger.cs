726bf7cc79e90461e6b484ef4d38742a61cc6046
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-11-18 00:50:56 +0100

    Almost forgot to make sure listener were properly disposed of.

--- Mandatory2DGameFramework/Logging/MyLogger.cs
+++ Mandatory2DGameFramework/Logging/MyLogger.cs
@@ -43,7 +43,7 @@ public sealed class MyLogger
     {
         foreach (TraceSource traceSource in _traceSources)
         {
-            traceSource.Listeners.Remove(listener);
+            RemoveListener(traceSource, listener);
         }
     }
 
@@ -51,7 +51,7 @@ public sealed class MyLogger
     {
         foreach (TraceSource traceSource in _traceSources)
         {
-            traceSource.Listeners.Remove(GetListener(traceSource, name));
+            RemoveListener(traceSource, GetListener(traceSource, name));
         }
     }
 
