614541b7fd58e72183bb0f8908eb78fc70a96482
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-11-18 05:45:03 +0100

    Quick refactor.

--- Mandatory2DGameFramework/model/Creatures/Creature.cs
+++ Mandatory2DGameFramework/model/Creatures/Creature.cs
@@ -7,6 +7,13 @@ using Mandatory2DGameFramework.Worlds;
 
 namespace Mandatory2DGameFramework.Model.Creatures;
 
+/// <summary>
+/// Base class for a creature living in a <see cref="World"/>.
+/// It serves as the basis for all players and NPCs in the game.
+/// </summary>
+/// <param name="name">Name of the creature.</param>
+/// <param name="position">The creatures initial position in world.</param>
+/// <param name="world">The world the creature is in.</param>
 public abstract class Creature(string name, WorldPosition position, World world) : WorldEntityBase(name, position, world), IHitSubject, IHitObserver
 {
     private readonly List<IHitObserver> _observers = [];
@@ -20,15 +27,36 @@ public abstract class Creature(string name, WorldPosition position, World world)
     public abstract List<DefenceItem> DefenceItems { get; set; }
     public abstract IItemHandler ItemHandler { protected get; set; } // Strategy pattern
 
+    /// <summary>
+    /// Calculates the distance for when a creature needs to move.
+    /// </summary>
+    /// <param name="directionVector"></param>
+    /// <returns>The length of the vector.</returns>
     private static int CalculateDistance(WorldPosition directionVector)
     {
         double distance = Math.Sqrt(Math.Pow(directionVector.X, 2) + Math.Pow(directionVector.Y, 2));
         return (int)Math.Floor(distance);
     }
 
+    public bool IsWithinWorld(WorldPosition position)
+    {
+        if ((position.X > World.MaxX && position.X >= 0) || (position.Y > World.MaxY && position.Y >= 0))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Moves this <see cref="Creature"/> 
+    /// </summary>
+    /// <param name="directionVector"></param>
+    /// <exception cref="ArgumentException"></exception>
     public void Move(WorldPosition directionVector)
     {
-        if (MoveRange == 0) { return; }
+        if (MoveRange <= 0) { return; }
+        WorldPosition newPosition = Position + directionVector;
+        if (!IsWithinWorld(newPosition)) { return; }
         int moveDistance = CalculateDistance(directionVector);
         if (moveDistance > MoveRange)
         {
@@ -36,17 +64,12 @@ public abstract class Creature(string name, WorldPosition position, World world)
                 "Movement vector must be less- or equal to the creature's movement range.",
                 nameof(directionVector));
         }
-        MoveTo(Position + directionVector);
+        Position = newPosition;
     }
 
-    private void MoveTo(WorldPosition newPosition)
+    public void MoveTo(WorldPosition newPosition)
     {
-        if (newPosition.X > World.MaxX || newPosition.Y > World.MaxY)
-        {
-            throw new ArgumentException(
-                $"New position {newPosition} exceeds world boundaries.",
-                nameof(newPosition));
-        }
+        if (!IsWithinWorld(newPosition)) { return; }
         Position = newPosition;
     }
 
