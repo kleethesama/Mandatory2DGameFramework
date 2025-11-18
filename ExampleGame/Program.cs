1e74d978acb4b2218e2e9d76dbc751fc337c3357
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-11-18 05:03:26 +0100

    Top level instead.

--- ExampleGame/Program.cs
+++ ExampleGame/Program.cs
@@ -6,98 +6,89 @@ using Mandatory2DGameFramework.Model.Creatures;
 using Mandatory2DGameFramework.Model.Items.Attack;
 using Mandatory2DGameFramework.Model.Items.BuffTree;
 using Mandatory2DGameFramework.Model.Items.Defence;
-using Mandatory2DGameFramework.Model.Items.Defence.Component;
-using Mandatory2DGameFramework.Model.Items.Defence.ComponentImplementation;
 using Mandatory2DGameFramework.Model.Items.Defence.Decorators;
+using Mandatory2DGameFramework.Model.Items.Defence.ComponentImplementation;
 using Mandatory2DGameFramework.Worlds;
 using System.Diagnostics;
 
-namespace ExampleGame;
-
-internal class Program
+TraceListener listener = new ConsoleTraceListener
 {
-    static void Main(string[] args)
-    {
-        TraceListener listener = new ConsoleTraceListener
-        {
-            Name = "ConfigManagerListener",
-            Filter = new EventTypeFilter(SourceLevels.All)
-        };
-        //MyLogger.Instance.AddListener(nameof(Creature), listener);
-        MyLogger.Instance.AddGlobalListener(listener);
+    Name = "ConfigManagerListener",
+    Filter = new EventTypeFilter(SourceLevels.All)
+};
+//MyLogger.Instance.AddListener(nameof(Creature), listener);
+MyLogger.Instance.AddGlobalListener(listener);
 
-        var world = new World();
-        var diff = GameDifficulty.Instance;
+var world = new World();
+var diff = GameDifficulty.Instance;
 
-        List<Configurator> configs = [];
-        configs.Add(new WorldSizeConfigurator(world));
-        configs.Add(new DifficultyConfigurator(diff));
+List<Configurator> configs = [];
+configs.Add(new WorldSizeConfigurator(world));
+configs.Add(new DifficultyConfigurator(diff));
 
-        ConfigManager.Instance.LoadConfigFile();
-        ConfigManager.Instance.ConfigureAll(configs);
+ConfigManager.Instance.LoadConfigFile();
+ConfigManager.Instance.ConfigureAll(configs);
 
-        WorldManager.SetWorld(world);
+WorldManager.SetWorld(world);
 
-        Console.WriteLine(WorldManager.Instance.CurrentWorld);
+Console.WriteLine(WorldManager.Instance.CurrentWorld);
 
-        Console.WriteLine(GameDifficulty.Instance);
+Console.WriteLine(GameDifficulty.Instance);
 
-        var factory = new BirdFactory(WorldManager.Instance.CurrentWorld);
-        List<Creature> creatures = factory.CreateRandoms(5);
-        //foreach (Creature creature in creatures)
-        //{
-        //    Console.WriteLine(creature);
-        //}
+var factory = new BirdFactory(WorldManager.Instance.CurrentWorld);
+List<Creature> creatures = factory.CreateRandoms(5);
+//foreach (Creature creature in creatures)
+//{
+//    Console.WriteLine(creature);
+//}
 
-        Creature coolBird = creatures[0];
-        Creature badBird = creatures[1];
+Creature coolBird = creatures[0];
+Creature badBird = creatures[1];
 
-        DefenceItem ringDefence = new(5, 0, "Cool Ring"); // Take less damage because it's cool.
-        AddDefence addBuff = new();
-        ringDefence.ApplyBuff(addBuff, 10);
-        Console.WriteLine(ringDefence);
+DefenceItem ringDefence = new(5, 0, "Cool Ring"); // Take less damage because it's cool.
+AddDefence addBuff = new();
+ringDefence.ApplyBuff(addBuff, 10);
+Console.WriteLine(ringDefence);
 
-        coolBird.AddDefenceItem(ringDefence);
+coolBird.AddDefenceItem(ringDefence);
 
-        AttackItem attackItem = new(20, 1, 1, "Sharp Claws");
-        badBird.AddAttackItem(attackItem);
+AttackItem attackItem = new(20, 1, 1, "Sharp Claws");
+badBird.AddAttackItem(attackItem);
 
-        // Move bad bird next to cool bird.
-        badBird.MoveRange = int.MaxValue;
-        badBird.Move(coolBird.Position - badBird.Position + new WorldPosition(1, 0));
-        Console.WriteLine($"Cool bird world position: {coolBird.Position}");
-        Console.WriteLine($"Bad bird world position: {badBird.Position}");
+// Move bad bird next to cool bird.
+badBird.MoveRange = int.MaxValue;
+badBird.Move(coolBird.Position - badBird.Position + new WorldPosition(1, 0));
+Console.WriteLine($"Cool bird world position: {coolBird.Position}");
+Console.WriteLine($"Bad bird world position: {badBird.Position}");
 
-        Console.WriteLine($"Cool bird start hitpoints: {coolBird.HitPoint}");
+Console.WriteLine($"Cool bird start hitpoints: {coolBird.HitPoint}");
 
-        // Bad bird gets notified if cool bird receieves a hit.
-        coolBird.Attach(badBird);
+// Bad bird gets notified if cool bird receieves a hit.
+coolBird.Attach(badBird);
 
-        // Bad bird attacks the cool bird (not a cool move).
-        badBird.Hit(coolBird);
-        Console.WriteLine($"Cool bird now has hitpoints: {coolBird.HitPoint}");
+// Bad bird attacks the cool bird (not a cool move).
+badBird.Hit(coolBird);
+Console.WriteLine($"Cool bird now has hitpoints: {coolBird.HitPoint}");
 
-        SuperDefenceBuff DoubleBuffAdded = new(addBuff); // Doubles the defence points and adds them.
-        ringDefence.ApplyBuff(DoubleBuffAdded, 3);
-        Console.WriteLine(ringDefence);
+SuperDefenceBuff DoubleBuffAdded = new(addBuff); // Doubles the defence points and adds them.
+ringDefence.ApplyBuff(DoubleBuffAdded, 3);
+Console.WriteLine(ringDefence);
 
-        badBird.Hit(coolBird);
-        Console.WriteLine($"Cool bird now has hitpoints: {coolBird.HitPoint}");
-        Console.WriteLine($"Cool bird is dead: {coolBird.IsDead()}");
+badBird.Hit(coolBird);
+Console.WriteLine($"Cool bird now has hitpoints: {coolBird.HitPoint}");
+Console.WriteLine($"Cool bird is dead: {coolBird.IsDead()}");
 
-        FragileDefenceDebuff halfDefenceDebuff = new(addBuff);
-        ringDefence.ApplyBuff(halfDefenceDebuff, ringDefence.ReduceHitPoint); // Something happened that made them half as cool.
-        Console.WriteLine(ringDefence);
+FragileDefenceDebuff halfDefenceDebuff = new(addBuff);
+ringDefence.ApplyBuff(halfDefenceDebuff, ringDefence.ReduceHitPoint); // Something happened that made them half as cool.
+Console.WriteLine(ringDefence);
 
-        badBird.Hit(coolBird);
-        Console.WriteLine($"Cool bird is dead: {coolBird.IsDead()}");
+badBird.Hit(coolBird);
+Console.WriteLine($"Cool bird is dead: {coolBird.IsDead()}");
 
-        BuffContainer container = new();
-        container.Add(addBuff);
-        BuffContainer nestedContainer = new();
-        nestedContainer.Add(DoubleBuffAdded);
-        container.Add(nestedContainer);
-        container.ApplyBuff(ringDefence, 50);
-        Console.WriteLine(ringDefence);
-    }
-}
+BuffContainer container = new();
+container.Add(addBuff);
+BuffContainer nestedContainer = new();
+nestedContainer.Add(DoubleBuffAdded);
+container.Add(nestedContainer);
+container.ApplyBuff(ringDefence, 50);
+Console.WriteLine(ringDefence);
